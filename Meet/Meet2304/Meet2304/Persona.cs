fa5f07fcc3ed6c4d0c8ac3101839971785cd879c
javierbeltran425 <00019315@uca.edu.sv>
2020-04-23 21:53:13 -0600

    Meet

---
+++ Meet/Meet2304/Meet2304/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Meet2304
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public Persona(string apellido = "Sin apellido", int edad = 0, string nombre = "Sin nombre")
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, {nameof(apellido)}: {apellido}, {nameof(edad)}: {edad}";
+        }
+        
+        public String Saludar(String saludoP)
+        {
+            return nombre + " " + saludoP;
+        }
+    }
+}
---
+++ Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //List<Persona> p = new List<Persona>();
+            var p = new List<Persona>();
+            p.Add(new Persona(nombre: "Walter", apellido: "Rafael", edad: 20));
+            p.Add(new Persona(nombre: "Batar", edad: 19));
+            p.Add(new Persona(edad: 22));
+            p.Add(new Persona(apellido: "Morales"));
+            
+            String buscar = "Walter";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre: ");
+                    it.Nombre = Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+
+            int añadir = 12;
+            int árbol = 14;
+        }
+    }
+}
